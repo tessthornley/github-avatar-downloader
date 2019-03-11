4f9db750bd4785149a87619814ee2dab78f27824
Tess <tess.thornley@gmail.com>
2019-03-11 23:33:28 +0000

    headers added

--- download-avatars.js
+++ download-avatars.js
@@ -1,9 +1,20 @@
 var request = require('request');
+var secrets = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'Token ' + secrets.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 //calling function with hardcoding
