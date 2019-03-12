1d38e8410bc5b5482492c981ba9622da142cbc09
Tess <tess.thornley@gmail.com>
2019-03-12 00:54:57 +0000

    hardcoded downloadImageByURL function added

--- download-avatars.js
+++ download-avatars.js
@@ -1,8 +1,10 @@
 var request = require('request');
 var secrets = require('./secrets.js');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// function to get user info
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -26,3 +28,20 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   // console.log("Result:", result)
   // commented out as specific results are now printed
 });
+
+// function to download URL
+function downloadImageByURL(url, filePath) {
+  request.get("https://avatars2.githubusercontent.com/u/2741?v=3&s=466")
+  .on('error', function(err){
+    throw err;
+  })
+  .on('response', function(response){
+    console.log('Downloading image...');
+    console.log('Response Messages ------------ ', response.statusMessage);
+    console.log('Status Type ---------- ', response.headers['content-type'])
+    console.log('Download complete.'); 
+  })
+  .pipe(fs.createWriteStream('./' + 'avatars/kvirani.jpg'));
+}
+
+downloadImageByURL();
